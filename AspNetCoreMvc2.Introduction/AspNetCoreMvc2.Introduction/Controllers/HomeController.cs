8536bff7bfa2076f39036e200f9998357d1bfa66
Yasin Aksu <yasinaksu88@gmail.com>
2020-07-31 14:18:38 +0300

    initial commit and first controller and first view

---
+++ AspNetCoreMvc2.Introduction/AspNetCoreMvc2.Introduction/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMvc2.Introduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from first application.";
+        }
+        public ViewResult Index2()
+        {
+            return View();
+        }
+    }
+}
